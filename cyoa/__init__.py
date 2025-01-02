0b86c3eb1e867c64402a4e33ee1f079be7d5469b
priscilla-b <pyfbaah@gmail.com>
2025-01-02 23:19:59 +0000

    resolve import issue

--- cyoa/__init__.py
+++ cyoa/__init__.py
@@ -5,7 +5,7 @@ import redis
 app = Flask(__name__, static_url_path='/static')
 app.config.from_pyfile('config.py')
 
-from config import REDIS_SERVER, REDIS_PORT, REDIS_DB
+from .config import REDIS_SERVER, REDIS_PORT, REDIS_DB
 redis_db = redis.StrictRedis(host=REDIS_SERVER, port=REDIS_PORT, db=REDIS_DB)
 
 socketio = SocketIO(app)
