04e10db10f5de896c9419ac6f7b37e276b6f451d
priscilla-b <pyfbaah@gmail.com>
2025-01-10 23:45:09 +0000

    fix circular imports

--- cyoa/__init__.py
+++ cyoa/__init__.py
@@ -2,9 +2,6 @@ from flask import Flask
 from flask_socketio import SocketIO
 import redis
 from .config import REDIS_SERVER, REDIS_PORT, REDIS_DB
-from . import views
-from . import websockets
-
 
 app = Flask(__name__, static_url_path='/static')
 app.config.from_pyfile('config.py')
@@ -12,4 +9,8 @@ app.config.from_pyfile('config.py')
 redis_db = redis.StrictRedis(host=REDIS_SERVER, port=REDIS_PORT, db=REDIS_DB)
 
 socketio = SocketIO(app)
+
+
+from . import views
+
 
